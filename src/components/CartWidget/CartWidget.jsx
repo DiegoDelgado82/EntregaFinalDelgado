01bd76636edded12a48ad4cbf55ac528ddf96d00
Diego Delgado <diegodelgado365@gmail.com>
2023-10-31 13:52:04 -0300

    armando proyecto

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -4,7 +4,7 @@ export const CartWidget = () => {
   return (
     <>
      <i className="fa fa-shopping-cart cartWidget"  href="#"></i>
-    <a id="numberCart">1</a>
+    <a id="numberCart">5</a>
     </>
     
   )
