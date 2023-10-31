ff2b82e487c128cf7faf25331819764dbcac4eb4
Diego Delgado <diegodelgado365@gmail.com>
2023-10-31 01:22:17 -0300

    primer commit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ export const NavBar = () => {
     <nav className="navbar navbar-expand-lg bg-body-tertiary navbar opacity-10">
         <div className="container-fluid">
           <a className="navbar-brand" href="index.html"
-            ><img id="logoBrand" src="./src/assets/img/logoBrand.png" alt="lmmogo"
+            ><img id="logoBrand" src="./src/assets/img/logoBrand.PNG" alt="Logo Centro Estudio"
           /></a>
 
           <button
