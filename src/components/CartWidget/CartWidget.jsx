db42acaec2cbb2aea02c81741a55c0fcb26a947e
Diego Delgado <diegodelgado365@gmail.com>
2023-11-01 08:37:10 -0300

    Modificando ItemListContainer

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,12 +1,15 @@
 
-
 export const CartWidget = () => {
   return (
-    <>
-     <i className="fa fa-shopping-cart cartWidget"  href="#"></i>
-    <a id="numberCart">5</a>
-    </>
-    
-  )
-}
-
+    <div className="container">
+      
+        <div className="col-12 d-flex justify-content-end">
+          <div className="d-flex align-items-end">
+            <i className="fa fa-shopping-cart cartWidget" href="#"></i>
+            <a className="numberCart">5</a>
+          </div>
+        </div>
+      </div>
+   
+  );
+};
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,18 @@
-
+import libro from "/libro.png"
 
 export const ItemListContainer = ({greeting}) => {
   return (
     <>
-    <div>Buen día {greeting} como estas?</div>
+ <div className="card  d-flex flex-column align-items-center" >
+      <img className="card-img-top w-25" src={libro} alt="Card image cap" />
+      <div className="card-body d-flex flex-column align-items-center">
+        <h5 className="card-title">Centro de estudios</h5>
+        <p className="card-text">Buen día {greeting} como estas hoy?</p>
+        <a href="#" className="btn btn-primary justify-content-center">Empecemos</a>
+        
+      </div>
+    </div>
+
     
     </>
     
