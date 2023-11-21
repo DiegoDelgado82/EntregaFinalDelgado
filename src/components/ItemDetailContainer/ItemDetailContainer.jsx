5c33d7e907ad57f01208eb6b3fdb14e2a7a8d9f6
Diego Delgado <diegodelgado365@gmail.com>
2023-11-21 11:55:53 -0300

    cargando comentarios

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { getProductById } from "../../../asyncMockProduct";
 import { ItemDetail } from "../ItemDetail/ItemDetail";
 import { useParams } from "react-router-dom";
-import cargando from "../../assets/img/cargando2.gif"
+import cargando from "../../assets/img/cargando.gif"
 //Llamamos al componente ItemDetail en el caso en que se encuentre el ID pasado por parametro
 export const ItemDetailContainer = () => {
     const { id } = useParams();
